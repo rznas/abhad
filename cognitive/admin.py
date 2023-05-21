ef3200b3b09498c2b8d7c64e4a499ef654f1f205
reza <rezanasri@outlook.com>
2023-05-21 18:02:15 +0330

    aa

--- cognitive/admin.py
+++ cognitive/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from jalali_date import datetime2jalali, date2jalali
+from jalali_date.admin import ModelAdminJalaliMixin, StackedInlineJalaliMixin, TabularInlineJalaliMixin
+from cognitive.models import Soldier, Skill, SoldierRelevant
 
 # Register your models here.
+
+@admin.register(Soldier)
+class SoldierAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    model = Soldier
+    list_display = ['name', 'rank', 'military_id', 'dispath_date_jalali', 'birth_date_jalali']
+    search_fields = ['name', 'rank', 'military_id', 'national_id', 'skills', 'education_field']
+    autocomplete_fields = ['skills']
+
+    @admin.display(description='تاریخ تولد', ordering='created')
+    def birth_date_jalali(self, obj: Soldier):
+        return date2jalali(obj.birth_date).strftime('%Y/%m/%d')
+    
+    @admin.display(description='تاریخ اعزام', ordering='created')
+    def dispath_date_jalali(self, obj: Soldier):
+        return date2jalali(obj.dispatch_date).strftime('%Y/%m/%d')
+    
+    @admin.display(description='نام و نشان', ordering='created')
+    def name(self, obj: Soldier):
+        return f'{obj.first_name} {obj.last_name}'
+
+
+@admin.register(Skill)
+class SkillModelAdmin(admin.ModelAdmin):
+    model = Skill
+    search_fields = ['name']
+
+
+@admin.register(SoldierRelevant)
+class BaseAdmin(admin.ModelAdmin):
+    pass
--- cognitive/models.py
+++ cognitive/models.py
@@ -59,23 +59,40 @@ class FAMILY_RELEVANCE_CHOICES(models.TextChoices):
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, verbose_name='نام')
     level = models.PositiveSmallIntegerField(
-        choices=LEVEL_CHOICES, default=LEVEL_CHOICES.MODERATE.value)
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='سطح')
+    
+    def __str__(self) -> str:
+        return f'{self.name} | {self.level}'
+    
+    class Meta:
+        verbose_name = 'مهارت'
+        verbose_name_plural = 'مهارت ها'
 
 
 class SoldierRelevant(models.Model):
     relevance = models.CharField(
-        max_length=1, choices=FAMILY_RELEVANCE_CHOICES.choices)
-    age = models.PositiveSmallIntegerField()
+        max_length=1, choices=FAMILY_RELEVANCE_CHOICES.choices, verbose_name='نسبت')
+    name = models.CharField(max_length=64, verbose_name='نام')
+    age = models.PositiveSmallIntegerField(verbose_name='سن')
     education_level = models.IntegerField(
-        choices=EDUCATION_LEVEL_CHOICES.choices)
-    job = models.CharField(max_length=64, null=True, blank=True)
+        choices=EDUCATION_LEVEL_CHOICES.choices, verbose_name='سطح تحصیلات')
+    job = models.CharField(max_length=64, null=True, blank=True, verbose_name='شغل')
+    is_alive = models.BooleanField(default=True, verbose_name='آیا در قید حیات هستند؟')
+
+    def __str__(self) -> str:
+        rel = self.get_relevance_display()
+        return f'{rel} | {self.name} ({self.age})'
+    
+    class Meta:
+        verbose_name = 'فرد نزدیک سرباز'
+        verbose_name_plural = 'نزدیکان سرباز'
 
 
 class Soldier(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE,
-                             related_name='soldier_profile', null=True, blank=True)
+    # user = models.ForeignKey(to=User, on_delete=models.CASCADE,
+    #                          related_name='soldier_profile', null=True, blank=True)
     first_name = models.CharField(max_length=64, verbose_name='نام')
     last_name = models.CharField(max_length=64, verbose_name='نشان')
     rank = models.IntegerField(
@@ -103,8 +120,41 @@ class Soldier(models.Model):
     cell_number = models.CharField(max_length=13, verbose_name='تلفن همراه')
     relevants = models.ManyToManyField(to=SoldierRelevant, verbose_name='بستگان')
     financial_status = models.PositiveSmallIntegerField(
-        choices=LEVEL_CHOICES, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت اقتصادی - مالی')
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت اقتصادی - مالی')
     belief_status = models.PositiveSmallIntegerField(
-        choices=LEVEL_CHOICES, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت مذهبی - اعتقادی')
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت مذهبی - اعتقادی')
     social_status = models.PositiveSmallIntegerField(
-        choices=LEVEL_CHOICES, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت اجتماعی')
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت اجتماعی')
+    social_status = models.PositiveSmallIntegerField(
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='وضعیت اجتماعی')
+    currently_disease = models.BooleanField(default=False, verbose_name='آیا اکنون بیماری خاصی دارید؟')
+    former_disease = models.BooleanField(default=False, verbose_name='آیا سابقه بیماری خاصی داشته اید؟')
+    head_accident_history = models.BooleanField(default=False, verbose_name='سابقه ضربه به سر و یا تصادف؟')
+    surgery_history = models.BooleanField(default=False, verbose_name='سابقه عمل جراحی؟')
+    drug_consumption_history = models.BooleanField(default=False, verbose_name='سابقه سو مصرف مواد مخدر؟')
+    being_poisoned_history = models.BooleanField(default=False, verbose_name='سابقه مسمومیت؟')
+    psycho_desease_history = models.BooleanField(default=False, verbose_name='سابقه اختلالات روانی؟')
+    medicine_treatment_history = models.BooleanField(default=False, verbose_name='سابقه درمان های دارویی اختلالات روانی؟')
+    psychotrapy_consultant_history = models.BooleanField(default=False, verbose_name='سابقه مشاوره و روان درمانی؟')
+    hospitalization_history = models.BooleanField(default=False, verbose_name='سابقه بستری شدن؟')
+    family_relation_with_parents_status = models.PositiveSmallIntegerField(
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='میزان روابط با والدین؟')
+    family_relation_with_siblings_status = models.PositiveSmallIntegerField(
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='میزان روابط با خواهر  برادر؟')
+    family_relation_with_each_other_status = models.PositiveSmallIntegerField(
+        choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.MODERATE.value, verbose_name='میزان روابط اعضای خانواده با یکدیگر؟')
+    divorce_history = models.BooleanField(default=False, verbose_name='سابقه جدایی والدین؟')
+    family_suicide_history = models.BooleanField(default=False, verbose_name='سابقه خودکشی در خانواده؟')
+    family_psychological_problems_history = models.BooleanField(default=False, verbose_name='سابقه بیماری های روانی در خانواده؟')
+    family_drug_consumption_history = models.BooleanField(default=False, verbose_name='سابقه سو مصرف مواد در خانواده؟')
+
+    interviewer_opinions = models.TextField(verbose_name='نظرات مصاحبه کننده', default='')
+
+    class Meta:
+        verbose_name = 'سرباز'
+        verbose_name_plural = 'سربازان'
+
+    def __str__(self):
+        rank = self.get_rank_display()
+        return f'{rank} وظیفه {self.first_name} {self.last_name}'
+
